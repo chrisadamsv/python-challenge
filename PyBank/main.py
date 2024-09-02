1c7fcad477bfcd1e37fc4b30b401c1fe4035df61
chrisadamsv <chrisadams@Chris-MacBook-Pro-16.local>
2024-09-02 05:02:19 -0400

    Adding homework assignment.

---
+++ PyBank/main.py
@@ -0,0 +1,76 @@
+# Import the os and csv modules into the script
+import os
+import csv
+
+# Create a path to the csv file to be used
+csvpath = os.path.join('Documents','August_Data_Analytics_Cohort','DataBootcamp','Homework-Repositories','python-challenge', 'PyBank', 'Resources', 'budget_data.csv')
+
+# Create a list to store the date values and the difference between the profits of each month
+dates = []
+profits_losses = []
+# Create variables to hold the values to be calculated i.e. total profit, the value of the previous month, the difference between the two, and the total months
+total_profit = 0
+dollar_value = 0
+difference = 0
+months = 1
+
+# Open up the csv file and skip the header and hold the first row of data in dollar_value, then add that to the total profit to start
+with open(csvpath, 'r', newline='' ) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+    first_row = next(csvreader)
+    dollar_value = int(first_row[1])
+    total_profit += dollar_value
+
+# Loop through all the rows after the header and first, store the dates in the list, calculate the difference between the two months, add the difference to the list...
+# ... change the dollar_value to the current row, and the current row to the total, and add the row to the months variable
+    for row in csvreader:
+        dates.append(row[0])
+        difference = (int(row[1]) - dollar_value) 
+        profits_losses.append(difference)
+        dollar_value = int(row[1])
+        total_profit = (total_profit + dollar_value)
+        months += 1
+
+# Calculate the average value with the numbers in the "profits_losses" list
+    average_difference = (sum(profits_losses)/len(profits_losses))
+
+# Calculate the month with the largest increase in profits, then grab the index of that value to pull the month
+    largest_increase = max(profits_losses)
+    l_i_index = profits_losses.index(largest_increase)
+    l_i_date = dates[l_i_index]
+
+# Calculate the month with the largest decrease in profits, then grab the index of that value to pull the month
+    largest_decrease = min(profits_losses)
+    l_d_index = profits_losses.index(largest_decrease)
+    l_d_date = dates[l_d_index]
+
+# Print the results of the code below: 
+print("-------------------------------------") 
+print("Financial Analysis")
+print("-------------------------------------") 
+print(f"Total Months: {months}") 
+print(f"Total: ${total_profit}")   
+print(f"Average Change: ${str(round(average_difference, 2))}")
+print(f"Greatest Increase In Profits: {l_i_date} (${largest_increase})")
+print(f"Greatest Decrease In Profits: {l_d_date} (${largest_decrease})")
+print("-------------------------------------") 
+
+# Store a list to hold all the lines to be exported to a .txt file
+lines = ["-------------------------------------",
+        "Financial Analysis",
+        "-------------------------------------",
+        (f"Total Months: {months}"),
+        (f"Total: ${total_profit}"),
+        (f"Average Change: ${str(round(average_difference, 2))}"),
+        (f"Greatest Increase In Profits: {l_i_date} (${largest_increase})"),
+        (f"Greatest Decrease In Profits: {l_d_date} (${largest_decrease})"),
+        "-------------------------------------"]
+
+# Dictate a path where the .txt file will go
+text_file_path = 'Documents/August_Data_Analytics_Cohort/DataBootcamp/Homework-Repositories/python-challenge/PyBank/Analysis/pybank.txt'
+
+# Create the .txt file and loop though the 'lines' list to export each line to a .txt file.
+with open(text_file_path, "w") as file:
+    for i in lines:
+        file.write(i + "\n")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-challenge
 For use with module 3 homework due 9.09
+To complete this challenge, I referenced ChatGBT and class files from the previous week.
